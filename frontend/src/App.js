de84c98d80401bdb44472bc2e4dc5e0e733fdc8c
kushwanth3103 <bollepallikushwanthsai@gmail.com>
2023-02-24 17:23:47 +0530

    Updated my part1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,38 @@
+<<<<<<< HEAD
 import Navbar from './Components/Common/Navbar';
 import Registraion from './Components/registrationForm';
 
+=======
+import logo from './logo.svg';
+import './App.css';
+>>>>>>> parent of 9121bb0 (added home component)
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
       <div className='navigation'>
       <Navbar/>
       </div>
       <div className='content'>
       <Registraion/>
       </div>
+=======
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+>>>>>>> parent of 9121bb0 (added home component)
     </div>
   );
 }
